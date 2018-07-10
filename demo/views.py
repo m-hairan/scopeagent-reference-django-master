8d6798d3326a0329fab2bfbe3c8e68f8eb36e0db
unitech1120 <uni.tech1120@gmail.com>
2018-07-09 18:01:20 -0700

    Remove requests dependency
    Signed-off-by: fermayo <fermayo@gmail.com>

--- demo/views.py
+++ demo/views.py
@@ -1,8 +1,5 @@
-import requests
-
 from django.http import HttpResponse
 
 
 def index(request):
-    usd = requests.get("https://api.coinmarketcap.com/v2/ticker/1/?convert=EUR").json()['data']['quotes']['USD']['price']
-    return HttpResponse("1 BTC = %.2f USD" % usd)
+    return HttpResponse("OK")
