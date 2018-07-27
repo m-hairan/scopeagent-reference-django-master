32ffc5c48470b5925544afb8f2f50f16ec192390
unitech1120 <uni.tech1120@gmail.com>
2018-07-27 15:55:34 -0700

    Unify tests in a single file
    Signed-off-by: Fernando Mayo <fermayo@gmail.com>

--- demo/tests.py
+++ demo/tests.py
@@ -1,8 +1,15 @@
 import unittest
 
+import codescope
+
 from demo.tasks import hello_world
 
 
 class UnitTests(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertEqual(hello_world(), "hello world!")
+
+
+class LiveTests(codescope.testing.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "hello world!")
--- demo/tests_live.py
+++
@@ -1,8 +0,0 @@
-from codescope.testing import TestCase
-
-from demo.tasks import hello_world
-
-
-class LiveTests(TestCase):
-    def test_hello_world(self):
-        self.assertEqual(hello_world(), "hello world!")
