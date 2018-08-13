2af1b5a5344460bb182c6f17d19cecb656a41514
unitech1120 <uni.tech1120@gmail.com>
2018-08-13 08:27:34 -0700

    Add logs to tests
    Signed-off-by: fermayo <fermayo@gmail.com>

--- demo/tests.py
+++ demo/tests.py
@@ -1,12 +1,18 @@
+import logging
 import unittest
 
 import codescope
 
 from demo.tasks import hello_world
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logging.basicConfig()
+
 
 class UnitTests(unittest.TestCase):
     def test_hello_world(self):
+        logger.info("hello world!")
         self.assertEqual(hello_world(), "hello world!")
 
 
